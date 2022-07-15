25e46a86ef059a0a6e7678e372f4c2f317006d80
sid8917 <102037657+sid8917@users.noreply.github.com>
2022-07-15 14:59:41 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    int countPaths(int m,int n,vector<vector<int>> &dp){
+         if(m==1||n==1){
+            return 1;
+        }
+        else if(dp[m][n]!=-1){
+            return dp[m][n];
+        }
+        else{
+            return dp[m][n]=countPaths(m-1,n,dp)+countPaths(m,n-1,dp);
+        }
+    }
     int uniquePaths(int m, int n) {
         //Solution 1 Brute Force
        /* if(m==1||n==1){
@@ -13,9 +24,10 @@ public:
           and for a new recursion call, we will compute and put the
           answer into the dp hashtable tc and sx O(m*n)  ....*/
         
-         vector<vector<int>> answer(m,(vector<int>(n,0)));
-        
-        for(int i=0;i<m;i++){
+      /* Solution2  o(m*n)tc and sc   
+      
+      vector<vector<int>> answer(m,(vector<int>(n,0)));
+       for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(i==0||j==0){
                     answer[i][j]=1;
@@ -26,9 +38,13 @@ public:
             }
         }
          
-        return answer[m-1][n-1];
+        return answer[m-1][n-1];*/
         
         
+        vector<vector<int>> dp(m+1,(vector<int>(n+1,-1)));
+        return countPaths(m,n,dp);
+        
+       
         
     }
 };
