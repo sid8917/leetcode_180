f8ad4e0b269ea45cdb4c6044eb7fe0f90a21731e
sid8917 <102037657+sid8917@users.noreply.github.com>
2022-10-02 00:40:00 +0530

    Time: 111 ms (86.45%), Space: 53.6 MB (17.12%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> umap;
+        
+        for(auto i:nums){
+            umap[i]++;
+        }
+        
+        for(auto i:umap){
+            if(i.second>1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
